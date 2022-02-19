379a1c4b3a85fe2f33833acdd53d023f75209a2c
dilusha07 <dilushasandaruwani07@gmail.com>
2022-02-19 22:12:08 +0530

    Create Product component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
+import React from "react";
+import data from "./data";
+import Product from "./components/Product";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <h1 style={{textAlign='center'}}>
-      Welcome to Page!
-    </h1>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">
+              Spice
+            </a>
+          </div>
+          <div>
+            <a href="cart.html">My Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div>
+            <div className="row center">
+              {data.products.map((product) => (
+                <Product key={product._id} product={product} />
+              ))}
+            </div>
+          </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
